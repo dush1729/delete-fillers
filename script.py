39a595a9f4361ab60b34345dfe9d706ba299cbe5
dush1729 <dush1729@gmail.com>
2021-08-22 21:19:51 +0530

    delete fillers

--- script.py
+++ script.py
@@ -22,3 +22,20 @@ for season in seasons:
 
 	os.chdir("..")
 	os.rmdir(season)
+
+print("Enter filler list in form 23 - 26, 34, 121 - 140")
+fillers_raw = input()
+for s in fillers_raw.split(", "):
+	l, r = 0, 0
+	if("-" in s):
+		idx = s.index("-")
+		l, r = int(s[:idx]), int(s[idx + 1:])
+	else:
+		l, r = int(s), int(s)
+
+	for filler_no in range(l, r + 1):
+		filler = str(filler_no) + ".mkv"
+		if(os.path.isfile(filler)):
+			os.remove(filler)
+
+print("fillers successfully removed!")
